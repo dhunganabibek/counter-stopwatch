81b3b2c16322c171bc5aa07a788b81f7e9cbff0d
Bibek Dhungana <bibek.dhungana@precisionplanting.com>
2023-09-03 01:28:58 -0500

    start and pause tick implemented

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello');
+class Timer{
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click',this.start);
+        this.pauseButton.addEventListener('click', this.stop);
+    }
+
+    start = () => { 
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    stop = () => {
+        clearInterval(this.interval);
+    }
+
+    tick = () => console.log('tick')     
+}
+
+
+const durationInput = document.querySelector('#duartion');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause')
+
+const timer = new Timer(durationInput, startButton, pauseButton);
